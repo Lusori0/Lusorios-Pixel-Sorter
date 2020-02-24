100d263fe8daba6c8717c087eeb0048d703589fb
Lusorio1 <sgt.lelant@gmail.com>
2020-02-24 19:12:03 +0100

    Inputfilename now wrapps, code cleanup

--- index.js
+++ index.js
@@ -2,192 +2,195 @@ const pixels = require('image-pixels');
 const output = require('image-output');
 const {app, BrowserWindow} = require('electron');
 const {ipcMain} = require('electron');
-const {dialog} = require('electron');
 
-var pixelList = [];
-var startPixel = [0,0];
-var imageData = [];
+//////////////////////////////////////
+////*Setup of everything electron*////
+//////////////////////////////////////
 
 console.log(app.getPath("temp"));
 
 app.on('ready', createWindow);
 app.on('windows-all-closed', () =>{
-    if(process.platform !== 'darwin'){
-        app.quit();
-    }
+	if(process.platform !== 'darwin'){
+		app.quit();
+	}
 });
 app.on('activate', () => {
-    if(win === null){
-        createWindow();
-    }
+	if(win === null){
+		createWindow();
+	}
 });
 
 let win;
 function createWindow(){
-    win = new BrowserWindow({
-        width:400,
-        height:600,
-        resizable: false,
-        frame: false,
-        webPreferences:{
-            nodeIntegration: true
-        }
-    });
-    win.loadFile('index.html');
+	win = new BrowserWindow({
+		width:400,
+	    height:600,
+		resizable: false,
+		frame: false,
+		webPreferences:{
+			nodeIntegration: true
+		}
+	});
+	win.loadFile('index.html');
 
-    win.on('closed', () => {
-        win = null;
-    });
+	win.on('closed', () => {
+		win = null;
+	});
 }
+/////////////////////////////////////////////////////////
+////*Communication between Main and Renderer process*////
+/////////////////////////////////////////////////////////
 
 ipcMain.on('startButton', (event, args) => {
-
-    getImage(args.inputFile, args.threshold, args.highlightFactor, args.noise, args.outputFile);
-    event.sender.send("btnclick-task-finished", "yes");
+	startPixelSorting(args.inputFile, args.threshold, args.highlightFactor, args.noise, args.outputFile);
+	event.sender.send("btnclick-task-finished", "yes");
 });
 
 ipcMain.on('closeApp',(event)=>{
-    app.quit();
+	app.quit();
 });
 
 ipcMain.on('minimizeApp', (event)=>{
-    win.minimize();
+	win.minimize();
 });
 
-async function getImage(inputFile, threshold, highlightFactor, noise, outputFile){
-    var {data, width, height} = await pixels(inputFile);
-    pixelSorting(data, width, height, threshold, highlightFactor, noise, outputFile);
+//////////////////////////////////////////////////////////////
+////*Make mask, rotate and overall apply Image Processing*////
+//////////////////////////////////////////////////////////////
+
+//Retrieve image data
+async function startPixelSorting(inputFile, threshold, highlightFactor, noise, outputFile){
+	var {data, width, height} = await pixels(inputFile);
+	pixelSorting(data, width, height, threshold, highlightFactor, noise, outputFile);
 };
 
-function sorting(width){
-    pixelList.sort((a,b) => sumOfPixel(a) - sumOfPixel(b));
-    for (p = 0; p < pixelList.length;p++){
-        imageData[(startPixel[1] + p - 1) * width + startPixel[0]] = pixelList[p];
-    }
-    pixelList = []
-    startPixel = [0,0];
+//Sort a list of Pixels and get it Back into imageData
+function sorting(width, pixelList, startPixel, imageData){
+	pixelList.sort((a,b) => sumOfPixel(a) - sumOfPixel(b));
+	for (p = 0; p < pixelList.length;p++){
+		imageData[(startPixel[1] + p - 1) * width + startPixel[0]] = pixelList[p];
+	}
+	return imageData;
 }
 
 function sumOfPixel(obj){
-    return obj.RGB.reduce((acc,curr) => acc + curr, 0) - 255;
+	return obj.RGB.reduce((acc,curr) => acc + curr, 0) - 255;
 }
 
 function pixelSorting(data, width, height, threshold, highlightFactor, noise, outputFile){
-    var mask = [];
+	var imageData = [];
+	var mask = [];
 
-    for (y = 0; y < height; y++){
-        for (x = 0; x < width; x++){
-            var rPos = (width*y+x)*4;
-            var gPos = (width*y+x)*4+1;
-            var bPos = (width*y+x)*4+2;
-            var pixelBrightness = data[rPos] + data[gPos] + data[bPos];
-            imageData.push({
-                "RGB":[
-                    data[rPos],
-                    data[gPos],
-                    data[bPos],
-                    255
-                ]
-            });
-            if(noise){
-                if(Math.random() < 0.998){
-                    mask.push({
-                        "RGB":[
-                            0,
-                            0,
-                            0,
-                            1
-                        ]
-                    });
-                }
-                else{
-                    mask.push({
-                        "RGB":[
-                            1,
-                            1,
-                            1,
-                            1
-                        ]
-                    });
-                }
-            }
-            if(pixelBrightness * highlightFactor > threshold * highlightFactor){
-                if(!noise){
-                   mask.push({
-                       "RGB":[
-                           0,
-                           0,
-                           0,
-                           1
-                       ]
-                   });
-                }
-            }
-            else{
-                if(noise){mask.pop();}
-                mask.push({
-                    "RGB":[
-                        1,
-                        1,
-                        1,
-                        1
-                    ]
-                });
-            }
+	for (y = 0; y < height; y++){
+		for (x = 0; x < width; x++){
+			var rPos = (width*y+x)*4;
+			var gPos = (width*y+x)*4+1;
+			var bPos = (width*y+x)*4+2;
+			var pixelBrightness = data[rPos] + data[gPos] + data[bPos];
+			imageData.push({
+				"RGB":[
+					data[rPos],
+					data[gPos],
+					data[bPos],
+					255
+				]
+			});
+			if(noise){
+				if(Math.random() < 0.998){
+					mask.push({
+						"RGB":[
+							0,
+							0,
+							0,
+							1
+						]
+					});
+				}
+				else{
+					mask.push({
+						"RGB":[
+							1,
+							1,
+							1,
+							1
+						]
+					});
+				}
+			}
+			if(pixelBrightness * highlightFactor > threshold * highlightFactor){
+				if(!noise){
+					mask.push({
+						"RGB":[
+							0,
+							0,
+							0,
+							1
+						]
+					});
+				}
+			}
+			else{
+				if(noise){mask.pop();}
+				mask.push({
+					"RGB":[
+						1,
+						1,
+						1,
+						1
+					]
+				});
+			}
 
-        }
-    }
-    var outputMask = [];
-    for(i = 0; i < mask.length;i++){
-        outputMask.push(mask[i].RGB[0]);
-        outputMask.push(mask[i].RGB[1]);
-        outputMask.push(mask[i].RGB[2]);
-        outputMask.push(mask[i].RGB[3]);
-    }
-    output({
-        data: outputMask,
-        width: width,
-        height: height
-    }, 'mask.png');
-    for (x = 0; x < width; x++){
-        for (y = 0; y < height; y++){
-            var [r, g, b, a] = imageData[width*y+x].RGB;
-            if(mask[width*y+x].RGB[0] == 0){
-                pixelList.push({
-                    "RGB":[
-                        r,
-                        g,
-                        b,
-                        255
-                    ]
-                });
-                if(startPixel[0] == 0 && startPixel[1] == 0){
-                    startPixel = [x,y];
-                }
-                if(y == height-1){
-                    sorting(width);
-                }
-            }
-            else{
-                if(pixelList.length > 0){
-                    sorting(width);
-                }
-            }
-        }
-    }
-    var outputImage = [];
-    for(i = 0; i < imageData.length;i++){
-        outputImage.push(imageData[i].RGB[0]);
-        outputImage.push(imageData[i].RGB[1]);
-        outputImage.push(imageData[i].RGB[2]);
-        outputImage.push(imageData[i].RGB[3]);
-    }
-    output({
-        data: outputImage,
-        width: width,
-        height: height
-    }, outputFile);
+		}
+	}
+	saveObjectAsImage(mask, width, height, 'mask.png');
+	var pixelList = [];
+	var startPixel = [0,0];
+	for (x = 0; x < width; x++){
+		for (y = 0; y < height; y++){
+			var [r, g, b, a] = imageData[width*y+x].RGB;
+			if(mask[width*y+x].RGB[0] == 0){
+				pixelList.push({
+					"RGB":[
+						r,
+						g,
+						b,
+						255
+					]
+				});
+				if(startPixel[0] == 0 && startPixel[1] == 0){
+					startPixel = [x,y];
+				}
+				if(y == height-1){
+					imageData =sorting(width, pixelList, startPixel, imageData);
+					pixelList = [];
+					startPixel = [0,0];
+				}
+			}
+			else{
+				if(pixelList.length > 0){
+					imageData = sorting(width, pixelList, startPixel, imageData);
+					pixelList = [];
+					startPixel = [0,0];
+				}
+			}
+		}
+	}
+	saveObjectAsImage(imageData, width, height, outputFile);
 }
 
-
-/*getImage();*/
+function saveObjectAsImage(input, width, height, name){
+	var outputData = [];
+	for(i = 0; i < input.length;i++){
+		outputData.push(input[i].RGB[0]);
+		outputData.push(input[i].RGB[1]);
+		outputData.push(input[i].RGB[2]);
+		outputData.push(input[i].RGB[3]);
+	}
+	output({
+		data: outputData,
+		width: width,
+		height: height
+	}, name);
+}
